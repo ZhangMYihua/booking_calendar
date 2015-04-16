c9e3bd9a0e45c3037c868711bcba284a781e2573
zhangmyihua <michaelzhang2@msn.com>
2015-04-16 16:17:52 -0400

    added models and timerange method

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,12 @@
 class Booking < ActiveRecord::Base
   belongs_to :user
   belongs_to :trainer
+
+  def timerange
+    TimeRange.new(start_time, end_time)
+  end
+
+
+
+
 end
--- app/models/timeslot.rb
+++ app/models/timeslot.rb
@@ -1,3 +1,7 @@
 class Timeslot < ActiveRecord::Base
   belongs_to :trainer
+
+  def timerange
+    TimeRange.new(start_time, end_time)
+  end
 end
